d2744508492f6822f8d036e2e30489822eb8f100
Jannis Poltier <jannis.poltier@gmail.com>
2024-12-11 11:56:52 +0100

    remove old code

--- manufacture-supplements/src/main/java/io/camunda/hu/exercise/manufacture_supplements/WorkerApplication.java
+++ manufacture-supplements/src/main/java/io/camunda/hu/exercise/manufacture_supplements/WorkerApplication.java
@@ -81,25 +81,4 @@ public class WorkerApplication {
 				.send()
 				.exceptionally( throwable -> { throw new RuntimeException("Could not complete job " + job, throwable); });
 	}
-	
-
-	// @JobWorker(type = "generate_certificate_of_recovery")
-	// public void generateCertificateOfRecovery(final ActivatedJob job, @Variable String person_uuid) {
-	// 	UUID recovery_certificate_uuid = UUID.randomUUID();
-	// 	logger.info("Generating certificate of recovery for person " + person_uuid +"...");
-	// 	logger.info("Generated certificate ID: " + recovery_certificate_uuid);
-	// 	logger.info("Storing Recovery Certificate in external database...");
-		
-	// 	client.newCompleteCommand(job.getKey())
-	// 			.variables("{\"recovery_certificate_uuid\": \"" + recovery_certificate_uuid + "\"}")
-	// 			.send()
-	// 			.exceptionally( throwable -> { throw new RuntimeException("Could not complete job " + job, throwable); });
-	// }
-
-	// @JobWorker(type = "send_certificate_of_recovery")
-	// public void sendCertificateOfRecovery(final ActivatedJob job, @Variable String person_uuid, @Variable String recovery_certificate_uuid) {
-	// 	logger.info("Retrieving Recovery Certificate " + recovery_certificate_uuid + " from external database...");
-	// 	logger.info("Retrieving contact details for person " + person_uuid + "from external database...");
-	// 	logger.info("Sending Recovery Certificate to person " + person_uuid + ". Enjoy that ice-cream!");
-	// }
 }
